1c3d5b45226a1a558ac8fe1aebb8b299aceb9a63
sonhosu <ghtn9398@nate.com>
2021-10-06 17:01:02 +0900

    초기세팅 및 Components , Props , State and Lifecycle

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Comment from './Comment';
+
+const comments = [
+  { name: 'Inje Lee', content: '안녕하세요' },
+  { name: '홍길동', content: '반갑습니다' },
+  { name: ' Lee', content: '안녕하세요!!' },
+]
+
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" style={{ padding: 16, backgroundColor: '#282c34' }}>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -18,6 +27,18 @@ function App() {
           Learn React
         </a>
       </header>
+      <div>
+        {comments.map((comment, index) => {
+          return(
+            <Comment
+              name={comment.name}
+              content={comment.content}
+            />
+          )
+        })}
+      </div>
+
+
     </div>
   );
 }
